a1fda9d9f6e6d3469e1f5bb795ba1e73839580fe
joseluisfiallosguerrero1 <jose.fiallos@unitec.edu>
2018-06-04 14:03:41 -0600

    fixing errors

--- src/binarios/bits.java
+++ src/binarios/bits.java
@@ -389,5 +389,207 @@ public class bits {
         }
         archivo.close();
     }
+    public boolean vim(long bloque, String nombre, String datos) throws FileNotFoundException, IOException{
+        long il = this.inodoLibre();
+        long bl = this.bloque_Libre();
+        int posI = 0;
+        int posF = 0;
+        ArrayList<Integer> bloquesUsados = new ArrayList();
+        ArrayList<String> SegmentosDatos = new ArrayList();
+        DirectoryEntrie direntrie = new DirectoryEntrie((int) il,nombre);
+        this.escribeBloqueDirEntrie( bloque, direntrie);
+        long tamañoArchivo = datos.length();
+        archivo = new RandomAccessFile("disco.bin", "rw");
+        archivo.seek(65537+il);
+        archivo.writeBoolean(false);
+        if(tamañoArchivo < 4096){
+            SegmentosDatos.add(datos);
+        }
+        if(tamañoArchivo > 4096 && tamañoArchivo < 8192){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095));
+        }
+        if(tamañoArchivo > 8192 && tamañoArchivo < 12288){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192));
+        }
+        if(tamañoArchivo > 12288 && tamañoArchivo < 16384){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288));
+        }
+        if(tamañoArchivo > 16384 && tamañoArchivo < 20480){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384));
+        }
+        if(tamañoArchivo > 20480 && tamañoArchivo < 24576){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480));
+        }
+        if(tamañoArchivo > 24576 && tamañoArchivo < 28672){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576));
+        }
+        if(tamañoArchivo > 28672 && tamañoArchivo < 32768){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576,28672));
+            SegmentosDatos.add(datos.substring(28672));
+        }
+        if(tamañoArchivo > 32768 && tamañoArchivo < 36864){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576,28672));
+            SegmentosDatos.add(datos.substring(28672,32768));
+            SegmentosDatos.add(datos.substring(32768));
+        }
+        if(tamañoArchivo > 36864 && tamañoArchivo < 40960){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576,28672));
+            SegmentosDatos.add(datos.substring(28672,32768));
+            SegmentosDatos.add(datos.substring(32768,36864));
+            SegmentosDatos.add(datos.substring(32768));
+        }
+        if(tamañoArchivo > 40960 && tamañoArchivo < 45056){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576,28672));
+            SegmentosDatos.add(datos.substring(28672,32768));
+            SegmentosDatos.add(datos.substring(32768,36864));
+            SegmentosDatos.add(datos.substring(32768,40960));
+            SegmentosDatos.add(datos.substring(40960));
+        }
+        if(tamañoArchivo > 45056 && tamañoArchivo < 49152){
+            SegmentosDatos.add(datos.substring(0,4095));
+            SegmentosDatos.add(datos.substring(4095,8192));
+            SegmentosDatos.add(datos.substring(8192,12288));
+            SegmentosDatos.add(datos.substring(12288,16384));
+            SegmentosDatos.add(datos.substring(16384,20480));
+            SegmentosDatos.add(datos.substring(20480,24576));
+            SegmentosDatos.add(datos.substring(24576,28672));
+            SegmentosDatos.add(datos.substring(28672,32768));
+            SegmentosDatos.add(datos.substring(32768,36864));
+            SegmentosDatos.add(datos.substring(32768,40960));
+            SegmentosDatos.add(datos.substring(40960,45056));
+            SegmentosDatos.add(datos.substring(45056));
+        }
+           
+        for(int i = 0;i<SegmentosDatos.size();i++){                               
+            
+            archivo.seek(bl);
+            archivo.writeBoolean(false);
+            bloquesUsados.add((int)bl);
+            archivo.seek(bl*4096);
+            archivo.writeUTF(SegmentosDatos.get(i));
+            bl = this.bloque_Libre();
+        }
+        for(int d =bloquesUsados.size();d<12;d++ ){
+            bloquesUsados.add(0);
+        }
+        Inodo inodo = new Inodo(2, (int) tamañoArchivo,0,bloquesUsados.get(0),bloquesUsados.get(1),bloquesUsados.get(2),bloquesUsados.get(3),bloquesUsados.get(4),bloquesUsados.get(5),bloquesUsados.get(6),bloquesUsados.get(7),bloquesUsados.get(8),bloquesUsados.get(9),bloquesUsados.get(10),bloquesUsados.get(11),0);
+        System.out.println(il);
+        archivo = new RandomAccessFile("disco.bin", "rw");
+        archivo.seek(65537+il);
+        archivo.seek(69633+(il*80));
+                archivo.writeInt(2);
+                archivo.writeInt(inodo.getI_size());
+                archivo.writeInt(0);
+                archivo.writeInt(inodo.getI_blocks1());
+                archivo.writeInt(inodo.getI_blocks2());
+                archivo.writeInt(inodo.getI_blocks3());
+                archivo.writeInt(inodo.getI_blocks4());
+                archivo.writeInt(inodo.getI_blocks5());
+                archivo.writeInt(inodo.getI_blocks6());
+                archivo.writeInt(inodo.getI_blocks7());
+                archivo.writeInt(inodo.getI_blocks8());
+                archivo.writeInt(inodo.getI_blocks9());
+                archivo.writeInt(inodo.getI_blocks10());
+                archivo.writeInt(inodo.getI_blocks11());
+                archivo.writeInt(inodo.getI_blocks12());
+                archivo.writeInt(0);
+                archivo.writeInt(inodo.getI_ctime());
+                archivo.writeInt(inodo.getI_time());
+                archivo.writeInt(inodo.getMtime());
+                archivo.writeInt(inodo.getI_dtime());
+        
+        archivo.close();
+        return true;
+    }
+    public String cat(String nombre,long bloque) throws FileNotFoundException, IOException{
+        Inodo inodo = new Inodo();
+        ArrayList<Long> bloques = new ArrayList();
+        String datos = "";
+        int cent = 0;
+        long dir = 0;
+            archivo = new RandomAccessFile("disco.bin", "rw");
+            archivo.seek(bloque*4096);
+            int id = archivo.readInt();
+            int tamn = archivo.readInt();
+            long tam = archivo.readLong();
+            String nom = archivo.readUTF();
+            
+            while(tam > 0 && cent < 4096){
+                cent+=80;
+                if(nombre.equals(nom)){
+                    
+                    archivo.seek(69633+(id*80));
+                    archivo.readInt();
+                    archivo.readInt();
+                    archivo.readInt();
+                    for (int i = 0;i<12;i++){
+                        dir = archivo.readInt();
+                        
+                        if(dir !=0 && dir < 65532){
+                            bloques.add(dir);
+                        }
+                    }
+                    
+                    for(int i = 0;i<bloques.size();i++){
+                        archivo.seek(bloques.get(i)*4096);
+                        datos = datos+archivo.readUTF();
+                    }
+                    archivo.close();
+                    return datos;
+                                    }
+                id=archivo.readInt();
+                archivo.readInt();
+                tam = archivo.readLong();
+                nom = archivo.readUTF();
+                
+            }
+            archivo.close();
+            return datos;
+    }
     
 }
